3b830855ee958d3c23562a75634a94abd02b5df2
naluthge <niroshaluthge@gmail.com>
2018-08-07 11:24:19 -0500

    Add existing file

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [ "$1" = "" ]; then
+	printf "\nProvide a link for USEARCH download (from email) as argument.\nGet a license from http://www.drive5.com/usearch/download.html\nSee RMarkdown file for details.\n\n"
+    exit 1
+fi
